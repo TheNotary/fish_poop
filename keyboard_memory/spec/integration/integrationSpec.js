2c06ca0def82f30c6129ba1392918099969717df
TheNotary <no@email.plz>
2018-09-04 09:20:10 -0500

    begins building a jasmin test that works in the browser

---
+++ keyboard_memory/spec/integration/integrationSpec.js
@@ -0,0 +1,19 @@
+// var window = new BrowserMock.Window();
+// var Screen = require("../../engine/screen.js");
+
+describe("Leveler", function() {
+
+  beforeEach(function() {
+    console.log("hello");
+  });
+
+
+  it("Leveler defaults to being level 0", function() {
+    expect(true).toBe(true);
+  });
+
+});
+
+
+
+
